63219171ed4a85437a11ea21cf6c8c0580e19916
Matthew Hinton <mhinton@gmail.com>
2023-12-27 10:11:54 -0600

    wip: Start on /blog

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+const navigation = appConfig.navigation;
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <div
+      class="bg-white" :class="{
+        isolate: $route.path === '/',
+      }"
+    >
+      <div class="px-6 py-6 lg:px-8">
+        <LandingNav :navigation="navigation" />
+        <LandingMobileNav :navigation="navigation" />
+      </div>
+    </div>
+    <div class="flex-1 container mx-auto z-0 px-4 py-6 lg:px-0">
+      <slot />
+    </div>
+    <LandingFooter />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
